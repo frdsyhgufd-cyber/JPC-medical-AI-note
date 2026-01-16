02d66c570118a6d4e4b78c80d5abd670a3d58652
frdsyhgufd-cyber <frdsyhgufd@gmail.com>
2026-01-16 16:19:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   define: {
+    // 讓 Netlify 的 API_KEY 環境變數能被前端 process.env.API_KEY 讀取
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
   build: {
